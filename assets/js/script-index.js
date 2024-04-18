1a9f5d4ff06db25d29d7c6a509ddb17140249479
devborgis <borgesv770@gmail.com>
2024-04-18 10:06:14 -0300

    corrigido links de contato e link para redirecionamento do projeto rlsBorgis

--- assets/js/script-index.js
+++ assets/js/script-index.js
@@ -1,6 +1,7 @@
 let abMenu = document.getElementById('ab-menu');
 let Menu = document.getElementById('menu-mobile');
 let overlayMenu = document.getElementById('overlay-menu');
+let rlsBorgis = document.getElementById('rlsBorgis');
 
 abMenu.addEventListener('click', ()=>{
     Menu.classList.add('abrir-menu')
@@ -12,4 +13,8 @@ Menu.addEventListener('click', ()=>{
 
 overlayMenu.addEventListener('click', ()=>{
     Menu.classList.remove('abrir-menu')
+});
+
+rlsBorgis.addEventListener('click', () => {
+    window.location.href = 'https://borgis.com.br/htmls/relatorios.html';
 });
